77910cee6531f348b21f0b0b53490ece4f1af647
Till Klocke <till.klocke@gmail.com>
2020-02-25 16:13:31 +0100

    Added example how krach can be used to securely serve http

---
+++ httpkrach_test.go
@@ -0,0 +1,70 @@
+package krach
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/connctd/noise"
+	"github.com/smolcert/smolcert"
+)
+
+func ExampleHTTPServer() {
+	serverAddr := "[::1]:8099"
+	rootCert, rootKey, err := smolcert.SelfSignedCertificate("example root CA",
+		time.Time{}, time.Time{}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	serverCert, serverKey, err := smolcert.SignedCertificate("example server cert",
+		2, time.Time{}, time.Time{}, nil, rootKey, rootCert.Subject)
+	if err != nil {
+		panic(err)
+	}
+
+	l, err := Listen(serverAddr, &ConnectionConfig{
+		StaticKey: noise.NewPrivateSmolIdentity(serverCert, serverKey),
+	}, smolcert.NewCertPool(rootCert))
+
+	doneChan := make(chan struct{}, 1)
+	go func() {
+		mux := http.NewServeMux()
+		mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello Krach"))
+		}))
+
+		http.Serve(l, mux)
+		<-doneChan
+	}()
+
+	clientCert, clientKey, err := smolcert.SignedCertificate("example client", 3, time.Time{},
+		time.Time{}, nil, rootKey, rootCert.Subject)
+	if err != nil {
+		panic(err)
+	}
+
+	krachTransport := http.Transport{}
+	krachTransport.Dial = func(network, addr string) (net.Conn, error) {
+		return Dial(addr, &ConnectionConfig{
+			StaticKey: noise.NewPrivateSmolIdentity(clientCert, clientKey),
+		}, smolcert.NewCertPool(rootCert))
+	}
+	client := &http.Client{
+		Transport: &krachTransport,
+	}
+
+	resp, err := client.Get("http://[::1]:8099/")
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	doneChan <- struct{}{}
+	fmt.Println(string(body))
+	// Output: Hello Krach
+}
