bd44e59a36163d9677f5f4bbf6dcb97ac5daf6d7
Till Klocke <till.klocke@gmail.com>
2020-02-25 13:16:50 +0100

    It was probably not the best idea to have the krach version byte as 0x00 by default. makes it harder to have an undefined value in the future

--- krach.go
+++ krach.go
@@ -16,7 +16,7 @@ var (
 
 const (
 	// KrachVersion is the byte representation of the currently supported wire protocol format for Krach
-	KrachVersion byte = 0x00
+	KrachVersion byte = 0x01
 )
 
 // configure the CBOR codec, so we serialize structs as arrays and have always the same byte representation of data
