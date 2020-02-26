225bbd8f88bcc1d60b4fa1dc66108af6c428be11
Till Klocke <till.klocke@gmail.com>
2020-02-26 13:42:39 +0100

    Added Identity defintions from noise fork. Semantically they belong more to this protocol

---
+++ smolcert.go
@@ -0,0 +1,58 @@
+package krach
+
+import (
+	"github.com/iost-official/ed25519/extra25519"
+	"github.com/smolcert/smolcert"
+	"golang.org/x/crypto/ed25519"
+)
+
+// SmolIdentity wraps a smolcert based certificate and provides the public
+// key as curve25519 representation instead of ed25519
+type Identity struct {
+	smolcert.Certificate
+}
+
+// PublicKey returns the curve25519 representation of the ed25519 public key of this certificate
+func (s *Identity) PublicKey() []byte {
+	// The certificates used by Smolcert use ed25519 keys, but we need curve25519 keys.
+	// As they both are based on the same curve, we can convert them.
+	var curvePubKey [32]byte
+	var edPubKey [32]byte
+	copy(edPubKey[:], s.Certificate.PubKey)
+	if !extra25519.PublicKeyToCurve25519(&curvePubKey, &edPubKey) {
+		// Signal that we couldn't create a valid curve25519 representation
+		return nil
+	}
+	return curvePubKey[:]
+}
+
+// Cert returns the plain smolcert certificate
+func (s *Identity) Cert() *smolcert.Certificate {
+	return &s.Certificate
+}
+
+// PrivateSmolIdentity wraps a SmolIdentity and an ed25519 private key
+type PrivateIdentity struct {
+	Identity
+	privKey ed25519.PrivateKey
+}
+
+// NewPrivateSmolIdentity creates a new PrivateSmolIdentity which contains the smolcert with the private key.
+// This might be needed for cryptographic operations like eDH or eDSA etc.
+func NewPrivateIdentity(cert *smolcert.Certificate, privKey ed25519.PrivateKey) *PrivateIdentity {
+	return &PrivateIdentity{
+		Identity: Identity{
+			Certificate: *cert,
+		},
+		privKey: privKey,
+	}
+}
+
+// PrivateKey returns a curve25519 representation of the private key
+func (p *PrivateIdentity) PrivateKey() []byte {
+	var edPrivKey [64]byte
+	var curvePrivKey [32]byte
+	copy(edPrivKey[:], p.privKey)
+	extra25519.PrivateKeyToCurve25519(&curvePrivKey, &edPrivKey)
+	return curvePrivKey[:]
+}
